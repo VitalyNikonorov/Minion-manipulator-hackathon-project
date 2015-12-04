13cce9bf4e0d444bf4f50705e0484a2ea6cbc20c
nik-vitaly <nik.vitaly@mail.ru>
2015-12-04 15:45:06 +0300

    add scan

--- app/src/main/java/net/nikonorov/behach/MainActivity.java
+++ app/src/main/java/net/nikonorov/behach/MainActivity.java
@@ -3,16 +3,53 @@ package net.nikonorov.behach;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.estimote.sdk.BeaconManager;
+
 
 /**
  * Created by vitaly on 04.12.15.
  */
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "myLog";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        beaconManager = new BeaconManager(this);
     }
+
+    private BeaconManager beaconManager;
+    private String scanId;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Should be invoked in #onStart.
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                scanId = beaconManager.startEddystoneScanning();
+                Log.d(TAG, scanId);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Should be invoked in #onStop.
+        beaconManager.stopEddystoneScanning(scanId);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // When no longer needed. Should be invoked in #onDestroy.
+        beaconManager.disconnect();
+    }
+
 }
