6f68bdff4289f6bd91bd70c3c9732390edcda985
nik-vitaly <nik.vitaly@mail.ru>
2015-12-04 15:15:35 +0300

    init

---
+++ app/src/main/java/net/nikonorov/behach/MainActivity.java
@@ -0,0 +1,18 @@
+package net.nikonorov.behach;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vitaly on 04.12.15.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
