5c3a5cc0a3926e28a9472d09d742b2bf2105d2e1
nik-vitaly <nik.vitaly@mail.ru>
2015-12-04 21:58:08 +0300

    UI

--- app/src/main/java/net/nikonorov/behach/MainActivity.java
+++ app/src/main/java/net/nikonorov/behach/MainActivity.java
@@ -4,8 +4,16 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.Collection;
 
 
 /**
@@ -15,11 +23,31 @@ public class MainActivity extends AppCompatActivity {
 
     String TAG = "myLog";
 
+    ImageButton buttonUp;
+    ImageButton buttonDown;
+    ImageButton buttonLeft;
+    ImageButton buttonRight;
+    ImageButton buttonPhoto;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         beaconManager = new BeaconManager(this);
+
+        buttonDown = (ImageButton)findViewById(R.id.arrow_down);
+        buttonUp = (ImageButton)findViewById(R.id.arrow_up);
+        buttonLeft = (ImageButton)findViewById(R.id.arrow_left);
+        buttonRight = (ImageButton)findViewById(R.id.arrow_right);
+        buttonPhoto = (ImageButton)findViewById(R.id.photo);
+
+
+        buttonDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Down Click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private BeaconManager beaconManager;
@@ -34,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             public void onServiceReady() {
                 scanId = beaconManager.startEddystoneScanning();
                 Log.d(TAG, scanId);
+
             }
         });
     }
@@ -51,5 +80,24 @@ public class MainActivity extends AppCompatActivity {
         // When no longer needed. Should be invoked in #onDestroy.
         beaconManager.disconnect();
     }
+//
+//        @Override
+//    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+//        for (Beacon beacon: beacons) {
+//            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+//                // This is a Eddystone-UID frame
+//                Identifier namespaceId = beacon.getId1();
+//                Identifier instanceId = beacon.getId2();
+//                Log.d("RangingActivity", "I see a beacon transmitting namespace id: " + namespaceId +
+//                        " and instance id: " + instanceId +
+//                        " approximately " + beacon.getDistance() + " meters away.");
+//                runOnUiThread(new Runnable() {
+//                    public void run() {
+//                        ((TextView)MainActivity.this.findViewById(R.id.tv_status)).setText("Hello world, and welcome to Eddystone!");
+//                    }
+//                });
+//            }
+//        }
+//    }
 
 }
