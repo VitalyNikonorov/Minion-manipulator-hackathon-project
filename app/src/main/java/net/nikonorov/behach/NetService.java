0c623b299bee8605897375e70d0a8a96bbb3b8ce
Bulat Khasanov <afti@yandex.ru>
2015-12-05 23:23:57 +0300

    last

--- app/src/main/java/net/nikonorov/behach/NetService.java
+++ app/src/main/java/net/nikonorov/behach/NetService.java
@@ -19,23 +19,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.Random;
 
 /**
  * Created by vitaly on 17.11.15.
  */
 public class NetService extends Service {
 
-    private int                     PORT    = 53000;
-    boolean                         isWork  = true;
+    private int PORT = 53000;
+    boolean isWork = true;
 
     private InputStream is;
     private BufferedOutputStream bout;
 
     private Socket socket;
 
-    final String                    LOG_TAG = "myLogs";
+    final String LOG_TAG = "myLogs";
 
     @Override
     public void onCreate() {
@@ -46,7 +47,7 @@ public class NetService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "doSomeCommand");
-        switch (intent.getIntExtra("type", -1)){
+        switch (intent.getIntExtra("type", -1)) {
             case (TaskType.CONNECT): {
                 task();
                 break;
@@ -58,7 +59,7 @@ public class NetService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void sendMessage(Intent intent){
+    private void sendMessage(Intent intent) {
         try {
             Log.d(LOG_TAG, "Try to send");
             Log.d(LOG_TAG, intent.getStringExtra("data"));
@@ -82,12 +83,12 @@ public class NetService extends Service {
         super.onDestroy();
     }
 
-    public void task(){
+    public void task() {
         Log.d(LOG_TAG, "TASK");
         new Connector().start();
     }
 
-    private class Connector extends Thread{
+    private class Connector extends Thread {
 
         @Override
         public void run() {
@@ -109,17 +110,16 @@ public class NetService extends Service {
         }
     }
 
-    public class Reader extends Thread{
+    public class Reader extends Thread {
 
-        Reader(){}
+        Reader() {
+        }
 
         @Override
         public void run() {
             int readBytes = 0;
-            byte[] buffer = new byte[256];
-            String temp = null;
-
-            StringBuilder sb = new StringBuilder();
+            byte[] buffer = new byte[1024];
+            byte[] accumulator = new byte[0];
             while (isWork) {
                 try {
                     readBytes = is.read(buffer);
@@ -127,7 +127,22 @@ public class NetService extends Service {
                     e.printStackTrace();
                 }
                 if (readBytes != -1) {
-                    temp = new String(buffer);
+                    Log.d("Read bytes", "Count: " + String.valueOf(readBytes));
+                    byte[] newbuf = new byte[buffer.length + accumulator.length];
+                    System.arraycopy(accumulator, 0, newbuf, 0, accumulator.length);
+                    System.arraycopy(buffer, 0, newbuf, accumulator.length, buffer.length);
+                    accumulator = newbuf;
+
+                    ByteBuffer wrapped = ByteBuffer.wrap(Arrays.copyOfRange(accumulator, 0, 2));
+                    short photoLength = wrapped.getShort();
+
+                    savePhoto(Arrays.copyOfRange(accumulator, 2, accumulator.length));
+                    /*
+                    byte[] c = new byte[a.length + b.length];
+                    System.arraycopy(a, 0, c, 0, a.length);
+                    System.arraycopy(b, 0, c, a.length, b.length);
+                    temp = temp + new String(buffer);
+                    */
                 }
             }
         }
@@ -142,7 +157,8 @@ public class NetService extends Service {
 
         public File getAlbumStorageDir(Context context, String albumName) {
             // Get the directory for the app's private pictures directory.
-            File file = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), albumName);
+            File root = Environment.getExternalStorageDirectory();
+            File file = new File(root, albumName);
             if (!file.mkdirs()) {
                 Log.e("PictureDemo", "Directory not created");
             }
@@ -159,8 +175,10 @@ public class NetService extends Service {
             }
 
             try {
+                Log.d("Photo", photo.getPath());
                 FileOutputStream fos = new FileOutputStream(photo.getPath());
-                fos.write(jpegBytes);
+                Log.d("Photo", String.valueOf(jpegBytes.length));
+                fos.write(jpegBytes, 0, jpegBytes.length);
                 fos.close();
             } catch (java.io.IOException e) {
                 Log.e("PictureDemo", "Exception in photoCallback", e);
